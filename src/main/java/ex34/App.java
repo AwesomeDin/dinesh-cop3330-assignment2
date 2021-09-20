8a5d4b441fa3c145cab64de5f4186345737e9925
Awesome <73072931+AwesomeDude357@users.noreply.github.com>
2021-09-19 20:47:57 -0400

    done

---
+++ src/main/java/ex34/App.java
@@ -0,0 +1,41 @@
+package ex34;
+
+/*
+ *  UCF COP3330 Fall 2021 Assignment 2 Solution
+ *  Copyright 2021 Rithvik Dinesh
+ */
+import java.lang.reflect.Array;
+import java.util.Scanner;
+import java.util.ArrayList;
+
+public class App {
+    public static void main(String[] args) {
+        ArrayList<String> currentEmployees = new ArrayList<>();
+        addEmp(currentEmployees);
+        System.out.println("There are 5 employees:");
+        for (String i : currentEmployees) {
+            System.out.println(i);
+        }
+        removeEmp(currentEmployees);
+        System.out.println("\nThere are 4 employees:");
+        for (String i : currentEmployees) {
+            System.out.println(i);
+        }
+
+    }
+    public static String nameRemove() {
+        Scanner myInput = new Scanner(System.in);
+        System.out.println("\nEnter an employee name to remove: ");
+        return myInput.nextLine();
+    }
+    public static void addEmp(ArrayList employees) {
+        employees.add("John Smith");
+        employees.add("Jackie Jackson");
+        employees.add("Chris Jones");
+        employees.add("Amanda Cullen");
+        employees.add("Jeremy Goodwin");
+    }
+    public static void removeEmp(ArrayList employees) {
+        employees.remove(employees.indexOf(nameRemove()));
+    }
+}
