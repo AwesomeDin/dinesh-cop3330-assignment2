7b410007645a997dc4882a52614a29aa3f7506f2
Awesome <73072931+AwesomeDude357@users.noreply.github.com>
2021-09-22 17:58:17 -0400

    done

--- src/main/java/ex27/App.java
+++ src/main/java/ex27/App.java
@@ -5,14 +5,18 @@ package ex27;
  *  Copyright 2021 Rithvik Dinesh
  */
 import java.util.Scanner;
+import java.util.regex.Pattern;
 
 public class App {
     public static void main(String[] args) {
+        //Uses a string array to get the information from the user
+        //It then sends this to the validate input function to figure everything out
         String [] theInfo = Controller();
         System.out.println(validateInput(theInfo[0],theInfo[1],theInfo[2],theInfo[3]));
     }
 
-    static String[] Controller() {
+    //Sets up the problem by getting all the information and sending it back to the main
+    public static String[] Controller() {
         String firstName = scanIn("Enter the first name: ");
         String lastName = scanIn("Enter the last name: ");
         String zipCode = scanIn("Enter the ZIP code: ");
@@ -20,12 +24,17 @@ public class App {
         String[] information = {firstName,lastName,zipCode,empId};
         return information;
     }
+
+    //Simple scanin function to get the input from the user.
     public static String scanIn(String s) {
         Scanner myInput = new Scanner(System.in);
         System.out.print(s);
         return myInput.next();
     }
-    static String validateInput(String first, String last, String ZIP, String emp) {
+
+    //Validates input by taking in the information and then sending it out to individual
+    //functions that use regex and if statements to make sure it follows the correct formatting
+    public static String validateInput(String first, String last, String ZIP, String emp) {
         String x = "";
         x += validateFirst(first);
         x+= validateLast(last);
@@ -37,7 +46,10 @@ public class App {
         }
         return x;
     }
-    static String validateFirst(String x) {
+
+    //Validates the first name, first make sures that the length is correct, then it
+    //makes sure that it is not blank.
+    public static String validateFirst(String x) {
         if(x.length() == 0) {
             return "The first name must be at least 2 characters long.\n" +
                     "The first name must be filled in.\n";
@@ -48,7 +60,10 @@ public class App {
         }
         return "";
     }
-    static String validateLast(String x) {
+
+    //Validates the last name, first make sures that the length is correct, then it
+    //makes sure that it is not blank.
+    public static String validateLast(String x) {
         if(x.length() == 0) {
             return "The last name must be at least 2 characters long.\n" +
                     "The last name must be filled in.\n";
@@ -59,15 +74,16 @@ public class App {
        }
         return "";
     }
-    static String validateZIP(String x) {
-        try {
-            int d = Integer.parseInt(x);
-        } catch (NumberFormatException nfe) {
-            return "The zipcode must be a 5 digit number.\n";
+    //Uses regex to check if the zipcode follows the correct formatting.
+    public static String validateZIP(String x) {
+        if(Pattern.matches("\\d\\d\\d\\d\\d",x)){
+            return "";
         }
-        return "";
+        else
+            return "The zipcode must be a 5 digit number.\n";
     }
-    static String validateEmp(String x) {
+    //Uses regex to check if the employeeID follows the correct formatting.
+    public static String validateEmp(String x) {
         if(x.matches("\\D\\D[-]\\d\\d\\d\\d"))
         {
             return "";
