2137b1914fbbc6981c879fad9cf1457b9ffd1479
Awesome <73072931+AwesomeDude357@users.noreply.github.com>
2021-09-22 17:41:43 -0400

    done

--- src/main/java/ex27/App.java
+++ src/main/java/ex27/App.java
@@ -8,22 +8,22 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
-
-        String finished = scanIn();
-        System.out.println(finished);
+        String [] theInfo = Controller();
+        System.out.println(validateInput(theInfo[0],theInfo[1],theInfo[2],theInfo[3]));
     }
-    static String scanIn() {
+
+    static String[] Controller() {
+        String firstName = scanIn("Enter the first name: ");
+        String lastName = scanIn("Enter the last name: ");
+        String zipCode = scanIn("Enter the ZIP code: ");
+        String empId = scanIn("Enter the employee ID: ");
+        String[] information = {firstName,lastName,zipCode,empId};
+        return information;
+    }
+    public static String scanIn(String s) {
         Scanner myInput = new Scanner(System.in);
-        System.out.print("Enter the first name: ");
-        String firstName = myInput.next();
-        System.out.print("Enter the last name: ");
-        String lastName = myInput.next();
-        System.out.print("Enter the ZIP code: ");
-        String zipCode = myInput.next();
-        System.out.print("Enter the employee ID: ");
-        String empId = myInput.next();
-        String result = validateInput(firstName,lastName,zipCode,empId);
-        return result;
+        System.out.print(s);
+        return myInput.next();
     }
     static String validateInput(String first, String last, String ZIP, String emp) {
         String x = "";
@@ -38,17 +38,24 @@ public class App {
         return x;
     }
     static String validateFirst(String x) {
-        if(x.length() < 2)
+        if(x.length() == 0) {
+            return "The first name must be at least 2 characters long.\n" +
+                    "The first name must be filled in.\n";
+        }
+        else if(x.length() < 2)
         {
             return "The first name must be at least 2 characters long.\n";
         }
         return "";
     }
     static String validateLast(String x) {
-       if(x.length() < 2)
+        if(x.length() == 0) {
+            return "The last name must be at least 2 characters long.\n" +
+                    "The last name must be filled in.\n";
+        }
+       else if(x.length() < 2)
        {
-           return "The last name must be at least 2 characters long.\n" +
-                   "The last name must be filled in.\n";
+           return "The last name must be at least 2 characters long.\n";
        }
         return "";
     }
--- src/test/java/ex27/AppTest.java
+++ src/test/java/ex27/AppTest.java
@@ -1,7 +1,33 @@
 package ex27;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
 
+    @Test
+    void validateInput() {
+        assertEquals("There were no errors found.",App.validateInput("John","Johnson","55555","TK-4321"));
+    }
+
+    @Test
+    void validateFirst() {
+        assertEquals("",App.validateFirst("John"));
+    }
+
+    @Test
+    void validateLast() {
+        assertEquals("",App.validateFirst("Johnson"));
+    }
+
+    @Test
+    void validateZIP() {
+        assertEquals("",App.validateZIP("55555"));
+    }
+
+    @Test
+    void validateEmp() {
+        assertEquals("",App.validateEmp("TK-4321"));
+    }
 }
