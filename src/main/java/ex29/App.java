711fd76a0219c41deae1a0209491bfa8fb9c9379
Awesome <73072931+AwesomeDude357@users.noreply.github.com>
2021-09-22 20:11:33 -0400

    done

--- src/main/java/ex29/App.java
+++ src/main/java/ex29/App.java
@@ -8,20 +8,32 @@ import java.util.Scanner;
 
 public class App {
     public static void main(String[] args) {
+        //Gets the result from the user and then rounds it up to make sure it covers a decimal
         int result = scanIn();
-        result  = 72/result;
+        result  = (int)Math.ceil((double)72/result);
         System.out.println("It will take " + result + " years to double your initial investment.");
     }
 
-    static int scanIn() {
+    //Scans in the initial number and then continues to ask until the user
+    //enters in a number then it uses a function to check if that
+    //number is a zero.
+    public static int scanIn() {
         Scanner myInput = new Scanner(System.in);
-        System.out.println("What is the rate of return? ");
+        System.out.print("What is the rate of return? ");
         while (!myInput.hasNextInt()) {
             System.out.println("Sorry. That's not a valid input.\n" +
                             "What is the rate of return?");
             myInput.next();
         }
         int result = myInput.nextInt();
+        result = checkforZero(result);
+        return result;
+    }
+
+    //Checks to see if the value passed is zero otherwise it will just return the
+    //number passed in.
+    public static int checkforZero(int result) {
+        Scanner myInput = new Scanner(System.in);
         while (result == 0) {
             System.out.println("Sorry. That's not a valid input.\n" +
                     "What is the rate of return?");
--- src/test/java/ex29/AppTest.java
+++ src/test/java/ex29/AppTest.java
@@ -1,7 +1,13 @@
 package ex29;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
 
+    @Test
+    void checkforZero() {
+        assertEquals(10,App.checkforZero(10));
+    }
 }
