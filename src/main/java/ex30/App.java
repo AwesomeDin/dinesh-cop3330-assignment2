9397528633f0d986eca9c402d4c018a27b73e725
Awesome <73072931+AwesomeDude357@users.noreply.github.com>
2021-09-22 20:19:20 -0400

    done

--- src/main/java/ex30/App.java
+++ src/main/java/ex30/App.java
@@ -4,18 +4,26 @@ package ex30;
  *  UCF COP3330 Fall 2021 Assignment 2 Solution
  *  Copyright 2021 Rithvik Dinesh
  */
-import java.util.Scanner;
 
 public class App {
+    //calls the function with the size of 12
     public static void main(String[] args) {
         printTable(12);
     }
+    //Used two for loops that would basically add as they go and used a modifier
+    //to make sure it stays in table format
     public static void printTable(int size) {
         for(int i = 1 ;i<=size;i++) {
             for(int j = 1;j<=size;j++) {
-                System.out.format("%5d",i*j);
+                System.out.format("%5d",multiply(i,j));
             }
             System.out.println();
         }
+    }
+
+    //Just multiplys the row and column of the nested loops since computations should have a method.
+    public static int multiply(int i, int j)
+    {
+        return i*j;
     }
 }
--- src/test/java/ex30/AppTest.java
+++ src/test/java/ex30/AppTest.java
@@ -1,7 +1,16 @@
 package ex30;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AppTest {
 
+    //This exercise didn't really have anything actually complicated since it was
+    //only printing which doesn't need a unit test so I just made the one computation there is a
+    //method so I could add a unit test.
+    @Test
+    void multiply() {
+        assertEquals(2,App.multiply(1,2));
+    }
 }
